1320843e2b723342a68a03a5436ca61b8e3bcc56
itsfrank98 <frybene@libero.it>
2022-03-11 20:44:17 +0100

    Still refactoring

--- LapSRN.py
+++ LapSRN.py
@@ -3,12 +3,15 @@ import math
 import numpy as np
 import itertools
 from VGGLoss import VGGLoss
-from keras.layers import Conv2DTranspose, Conv2D, LeakyReLU
+from keras.layers import Conv2DTranspose, Conv2D, LeakyReLU, Input
+from keras.models import Model
 from keras.utils.generic_utils import get_custom_objects
+from tensorflow.keras.optimizers import schedules
 
 class LapSRN:
     def __init__(self, input, scale, batch_size, learning_rate, alpha, vgg_layer='block5_conv4', input_shape=(128, 128)):
         self.LR_input = input   # Low resolution inputs
+        self.input_shape = input_shape
         self.batch_size = batch_size
         self.scale = int(scale)
         self.num_of_components = int(math.floor(math.log(self.scale, 2)))
@@ -17,14 +20,15 @@ class LapSRN:
         self.filter_initializer = tf.keras.initializers.GlorotNormal()
         self.bias_initializer = tf.constant_initializer(value=0.1)
         self.outputs = list()
-        self.loss = VGGLoss(input_shape, vgg_layer)
+        self.loss = VGGLoss(vgg_layer)
 
     def activation(self, layer):
         return tf.nn.relu(layer) - 0.2 * tf.nn.relu(-1 * layer)
 
     def subpixel(self, X: tf.Tensor, upscale_factor):
         # Implementation of subpixel layer provided on https://neuralpixels.com/subpixel-upscaling/
-        batch_size, rows, cols, in_channels = X.get_shape().as_list()
+        shape = X.get_shape().as_list()
+        batch, rows, cols, in_channels = [1, shape[1], shape[2], shape[3]]
         kernel_filter_size = upscale_factor
         out_channels = int(in_channels // (upscale_factor * upscale_factor))
 
@@ -39,16 +43,17 @@ class LapSRN:
                 i += 1
 
         new_rows, new_cols = int(rows * upscale_factor), int(cols * upscale_factor)
-        new_shape = [batch_size, new_rows, new_cols, out_channels]
+        new_shape = [batch, new_rows, new_cols, out_channels]
         tf_shape = tf.stack(new_shape)
         strides_shape = [1, upscale_factor, upscale_factor, 1]
         out = tf.nn.conv2d_transpose(X, kernel, tf_shape, strides_shape, padding='VALID')
+        out = tf.expand_dims(tf.reduce_mean(out, axis=-1), axis=-1)
         return out
 
     def deconv_layer_pixel_shuffle(self, input_layer, channels):
         """Layer used for upsampling the image"""
         upscale_factor = 2
-        deconv_layer = Conv2D(filters=upscale_factor*upscale_factor*channels, kernel_size=4, strides=[1, 1, 1, 1], padding='SAME', data_format='NHWC')(input_layer)
+        deconv_layer = Conv2D(filters=upscale_factor*upscale_factor*channels, kernel_size=4, strides=(1, 1), padding='SAME')(input_layer)
         deconv_layer = tf.nn.depth_to_space(deconv_layer, upscale_factor, data_format='NHWC')
         return deconv_layer
 
@@ -66,33 +71,36 @@ class LapSRN:
         """
         layer_fe = LeakyReLU(alpha=self.alpha)(input_layer)
         for i in range(1, 10):
-            layer_fe = Conv2D(filters=64, kernel_size=3, strides=[1, 1, 1, 1])(layer_fe)
+            layer_fe = Conv2D(filters=64, kernel_size=3, strides=(1, 1), padding='SAME')(layer_fe)
             layer_fe = LeakyReLU(alpha=self.alpha)(layer_fe)
         layer_fe += input_layer     # Residual connection
         layer_fe = self.feature_upsample_layer(layer_fe, 2)
         return layer_fe
 
     def LapSRN_model(self):
-        outputs = list()
-        prev_fe_layer = self.LR_input
-        prev_re_layer = self.LR_input
-
+        input_layer = Input((self.input_shape[0], self.input_shape[1], 1))
+        prev_fe_layer = input_layer
+        prev_re_layer = input_layer
         for n in range(0, self.num_of_components):
-            current_scale = pow(2, (n + 1))
             fe_output = self.feature_extraction_block(prev_fe_layer)
             upsampled_image = self.image_upsample_layer(prev_re_layer, 1)
-            re_output = self.activation(tf.math.add(upsampled_image, fe_output))
-
+            re_output = tf.math.add(upsampled_image, fe_output)
             prev_fe_layer = fe_output
             prev_re_layer = re_output
-            outputs.append(re_output)
+        m = Model(input_layer, prev_re_layer)
+        return m
 
-            output_name = "NCHW_output_" + str(current_scale) + "x"
-            tf.transpose(re_output, [0, 3, 1, 2], name=output_name)
+    def prepare_model(self):
+        def psnr(im1, im2):
+            return tf.image.psnr(im1, im2, max_val=3.0)
 
-        return outputs
+        model = self.LapSRN_model()
+        schedule = schedules.ExponentialDecay(self.learning_rate, 10000, 0.95, staircase=True)
+        opt = tf.keras.optimizers.Adam(learning_rate=schedule)
+        model.compile(optimizer=opt, loss=self.loss.compute_loss, metrics=[psnr], run_eagerly=True)
+        return model
 
-    def LapSRN_trainable_model_multi(self, HR_outputs, HR_origs):
+    '''def LapSRN_trainable_model_multi(self, HR_outputs, HR_origs):
         losses = list()
         train_ops = list()
         psnrs = list()
@@ -115,9 +123,31 @@ class LapSRN:
         psnr = tf.image.psnr(HR_out, HR_orig, max_val=1.0)
 
         loss = self.loss.compute_loss(HR_out, HR_orig)
-        decayed_lr = tf.keras.optimizers.schedule.ExponentialDecay(self.learning_rate, 10000, 0.95, staircase=True)
+        decay_schedule = tf.keras.optimizers.schedule.ExponentialDecay(self.learning_rate, 10000, 0.95, staircase=True)
         # decayed_lr = tf.train.exponential_decay(self.learning_rate, self.global_step, 10000, 0.95, staircase=True)
-        train_op = tf.keras.optimizers.Adam(learning_rate=decayed_lr).minimize(loss)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=decay_schedule)
 
-        return loss, train_op, psnr
+        return loss, train_op, psnr'''
 
+'''
+from LapSRN import LapSRN
+import numpy as np
+from tensorflow.keras.optimizers import schedules
+import tensorflow as tf
+
+lr_im = "dataset/train/lr_x2/train/lr_10_0.npy"
+hr_im = "dataset/train/gt/x2_train/hr_10_0.npy"
+with open(lr_im, 'rb') as f:
+    im = np.load(f)
+
+with open(hr_im, 'rb') as f:
+    im_hr = np.load(f)
+
+c = LapSRN(im, 2, 1, 0.007, 2)
+decay_schedule = schedules.ExponentialDecay(c.learning_rate, 10000, 0.95, staircase=True)
+opt = tf.keras.optimizers.Adam(learning_rate=decay_schedule)
+m = c.prepare_model()
+im = np.expand_dims(im, axis=(0, -1))
+im_hr = np.expand_dims(im_hr, axis=(0, -1))
+m.fit(im, im_hr, batch_size=1, epochs=1)
+'''
--- VGGLoss.py
+++ VGGLoss.py
@@ -1,12 +1,13 @@
 from keras.applications.vgg19 import VGG19
 from keras.models import Model
+from keras.layers import Concatenate
 import tensorflow as tf
 
 
 class VGGLoss:
-    def __init__(self, input_shape, feat_extraction_layer='block5_conv4'):
-        self.vgg = VGG19(include_top=False, input_shape=input_shape)
-        self.model = Model(inputs=self.vgg.inputs, outputs=self.vgg.get_layer(feat_extraction_layer).outputs)
+    def __init__(self, feat_extraction_layer):
+        self.vgg = VGG19(include_top=False, input_shape=(256, 256, 3))
+        self.model = Model(inputs=self.vgg.inputs, outputs=self.vgg.get_layer(feat_extraction_layer).output)
 
     def compute_loss(self, predicted_image, ground_truth_image):
         """
@@ -18,13 +19,9 @@ class VGGLoss:
         :param ground_truth_image:
         :return:
         """
-        img_lr_tens = tf.constant([predicted_image, predicted_image, predicted_image])
-        img_lr_tens = tf.reshape(img_lr_tens, (1, img_lr_tens.shape[1], img_lr_tens.shape[2], img_lr_tens.shape[0]))
-
-        img_hr_tens = tf.constant([ground_truth_image, ground_truth_image, ground_truth_image])
-        img_hr_tens = tf.reshape(img_hr_tens, (1, img_hr_tens.shape[1], img_hr_tens.shape[2], img_hr_tens.shape[0]))
-
-        img_lr_feats = self.model(img_lr_tens)
-        img_hr_feats = self.model(img_hr_tens)
-
-        return tf.reduce_mean(tf.subtract(img_hr_feats, img_lr_feats)**2, [1, 2, 3]).numpy[0]
+        img_predicted_tens = Concatenate()([predicted_image, predicted_image, predicted_image])
+        img_gt_tens = Concatenate()([ground_truth_image, ground_truth_image, ground_truth_image])
+        img_lr_feats = self.model(img_predicted_tens)
+        img_hr_feats = self.model(img_gt_tens)
+        c = tf.reduce_mean(tf.subtract(img_hr_feats, img_lr_feats)**2, [1, 2, 3])
+        return tf.reduce_mean(tf.subtract(img_hr_feats, img_lr_feats)**2, [1, 2, 3])
